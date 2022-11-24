cfb3f9e2901521651fea7de631b92ee79e8714d7
Javier Cepeda <erjavierc@gmail.com>
2022-11-23 21:10:51 -0500

    adding movement to my snake

--- snake/src/model/state.rb
+++ snake/src/model/state.rb
@@ -1,4 +1,11 @@
 module Model
+  module Direction
+    UP = :up
+    RIGHT = :right
+    DOWN = :down
+    LEFT = :left
+  end
+
   class Coord < Struct.new(:row, :col)
   end
 
@@ -11,17 +18,19 @@ module Model
   class Grid < Struct.new(:rows, :cols)
   end
 
-  class State < Struct.new(:snake, :food, :grid)
+  class State < Struct.new(:snake, :food, :grid, :next_direction, :game_finished)
   end
 
-  def self.inital_state
+  def self.initial_state
     Model::State.new(
-      Model::Snake.new([
-        Model::Coord.new(1,1),
-        Model::Coord.new(0, 1)
-      ]),
-      Model::Food.new(4, 4),
-      Model::Grid.new(8, 12)
+        Model::Snake.new([
+            Model::Coord.new(1,1),
+            Model::Coord.new(0,1)
+        ]),
+        Model::Food.new(4, 4),
+        Model::Grid.new(8, 12),
+        Direction::DOWN,
+        false
     )
   end
 end
