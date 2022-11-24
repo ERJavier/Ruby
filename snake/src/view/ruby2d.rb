0c4740f8689646b44df0aa2d5a541e83d54ca148
Javier Cepeda <erjavierc@gmail.com>
2022-11-23 22:10:17 -0500

    making my app function properly..

--- snake/src/view/ruby2d.rb
+++ snake/src/view/ruby2d.rb
@@ -1,48 +1,51 @@
 require "ruby2d"
-require_relative "../model/state"
 
 module View
-  class Ruby2dView
+    class Ruby2dView 
 
-    def initialize
-      @pixel_size = 50
+        def initialize
+            @pixel_size = 50
+        end
+
+        def start (state)
+            extend Ruby2D::DSL
+            set(
+                title: "Snake", 
+                width: @pixel_size * state.grid.cols, 
+                height: @pixel_size * state.grid.rows)
+            show
+        end
+
+        def renderGame(state)
+            render_food(state)
+            render_snake(state)
+        end
+
+        private
+        def render_food(state)
+            @food.remove if @food
+            extend Ruby2D::DSL
+            food = state.food
+            @food = Square.new(
+                x: food.col * @pixel_size, 
+                y: food.row * @pixel_size,
+                size: @pixel_size,
+                color: 'orange',
+            )
+        end
+
+        def render_snake(state)
+            @snake_positions.each(&:remove ) if @snake_positions
+            extend Ruby2D::DSL
+            snake = state.snake
+            @snake_positions = snake.positions.map do |pos|
+                Square.new(
+                    x: pos.col * @pixel_size, 
+                    y: pos.row * @pixel_size,
+                    size: @pixel_size,
+                    color: 'blue',
+                )
+            end
+        end
     end
-
-    def render(state)
-      extend Ruby2D::DSL
-      set(
-          title: "Snake", 
-          width: @pixel_size * state.grid.cols,
-          height: @pixel_size * state.grid.rows)
-      render_food(state)
-      render_snake(state)
-      show
-    end
-
-    private
-
-    def render_food(state)
-      extend Ruby2D::DSL
-      food = state.food
-      Square.new(
-        x: food.col * @pixel_size,
-        y: food.row * @pixel_size,
-        size: @pixel_size,
-        color: 'yellow'
-      )
-    end
-
-    def render_snake(state)
-      extend Ruby2D::DSL
-      snake = state.snake
-      snake.positions.each do |pos|
-        Square.new(
-          x: pos.col * @pixel_size,
-          y: pos.row * @pixel_size,
-          size: @pixel_size,
-          color: 'green'
-        )
-      end
-    end
-  end
 end
