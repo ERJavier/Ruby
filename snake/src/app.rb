cbcf3b5cebb031b8dff004b55d62645d60b828a2
Javier Cepeda <erjavierc@gmail.com>
2022-11-24 21:25:36 -0500

    adding movemnt to the game

--- snake/src/app.rb
+++ snake/src/app.rb
@@ -3,23 +3,23 @@ require_relative "model/state"
 require_relative "actions/actions"
 
 class App
-    def initialize
-        @state = Model::initial_state
-    end
+  def initialize
+    @state = Model::initial_state
+  end
 
-    def start
-        view = View::Ruby2dView.new
-        Thread.new { init_timer(view) }
-        view.start(@state)
-    end
+  def start
+    view = View::Ruby2dView.new
+    Thread.new { init_timer(view) }
+    view.start(@state)
+  end
 
-    def init_timer(view)
-        loop do
-            @state = Actions::move_snake(@state)
-            view.renderGame(@state)  
-            sleep 0.5
-        end
+  def init_timer(view)
+    loop do
+      @state = Actions::move_snake(@state)
+      view.render(@state)
+      sleep 0.5
     end
+  end
 end
 
 app = App.new
--- snake/src/view/ruby2d.rb
+++ snake/src/view/ruby2d.rb
@@ -4,9 +4,8 @@ require_relative "../model/state"
 module View
   class Ruby2dView
 
-    def initialize(app)
+    def initialize
       @pixel_size = 50
-      @app = app
     end
 
     def start(state)
@@ -15,10 +14,6 @@ module View
           title: "Snake", 
           width: @pixel_size * state.grid.cols,
           height: @pixel_size * state.grid.rows)
-      on :key_down do |event|
-        # A key was pressed
-        handle_key_event(event)
-      end
       show
     end
 
@@ -54,23 +49,5 @@ module View
         )
       end
     end
-
-    def handle_key_event(event)
-      case event.key
-      when "up"
-        # cambiar direccion hacia arriba
-        @app.send_action(:change_direction, Model::Direction::UP)
-      when "down"
-        # cambiar direccion hacia abajo
-        @app.send_action(:change_direction, Model::Direction::DOWN)
-      when "left"
-        # cambiar direccion hacia izquierda
-        @app.send_action(:change_direction, Model::Direction::LEFT)
-      when "right"
-        # cambiar direccion hacia derecha
-        @app.send_action(:change_direction, Model::Direction::RIGHT)
-      end
-    end
   end
-
 end
